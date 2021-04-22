90f7adf4a6f4b3ab54d207b61643262bbef7450c
Pragadeesh-Raj <82992915+Pragadeesh-Raj@users.noreply.github.com>
2021-04-22 09:40:19 +0530

    Add files via upload

---
+++ instaprofile.py
@@ -0,0 +1,11 @@
+import os
+import instaloader
+
+def download(username):
+    instance = instaloader.Instaloader()
+    os.chdir(os.path.join(os.path.expanduser("~"), "Desktop"))
+    return instance.download_profile(username, profile_pic_only=True)
+
+if __name__ == "__main__":
+    username = input("Enter Username: ")
+    download(username)
